ce64fd1dff04d665df79e1d47d8090f358576f25
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2019-01-13 13:59:07 -0500

    Moved to be a better situation

--- Module-01/Variables-and-Conditionals/main.js
+++ Module-01/Variables-and-Conditionals/main.js
@@ -1,47 +1,59 @@
 // Step 1: Create a variable with the symbol name
 // 'greeting' and assign it the value of 'Hello World'
-greeting = "lajsdfj"
+
 
 // Step 2: Create a variable with the symbol name
 // 'fullName' and assign it the value of your name
 
+
 // Step 3: Create a variable with the symbol name
 // 'age' and assign it the value of your age
+
 
 // Step 4: Create a variable with the symbol name
 // 'hourlyIncome' and assign it a decimal value
 
+
 // Step 5: Create a variable with the symbol name
 // 'married' and assign it a boolean value
+
 
 // Step 6: Create a variable with the symbol name
 // 'favFood' and assign it the string value of your
 // favourite food. Give it only block scoped visibility
 // and make it mutable.
 
+
 // Step 7: Create a variable with the symbol name
 // 'favColour' and assign it the string value of your
 // favourite colour. Give it only functional scoped
 // visibility and make it mutable.
+
 
 // Step 8: Create a variable with the symbol name
 // 'dob' and assign it the string value of your
 // birthdate. Give it only block scoped
 // visibility and make it immutable.
 
+
 // Step 9: Create a variable with the symbol name
 // 'favNum' and assign it the number value of your
 // favourite number. Give it only global visibility
 // and make it mutable.
 
+
 // Step 10: Compare 5 and "5" so it is true using loose comparison
 let q;
+
 // Step 11: Compare 7 and 3 so it is true using greater than
 let w;
+
 // Step 12: Compare "Shaun" and "Bob" so it is true using loose comparison
 let e;
+
 // Step 13: Compare 5 and "5" so it is false using strict comparison
 let r;
+
 // Step 14: Compare both expressions so it's true
 // using the correct logical operator
 let expa1 = 5 < 6;
@@ -62,6 +74,7 @@ let ourTestNum = 10;
 //    to 20.
 // c) Otherwise, set ourTestNum to 15
 
+
 // Step 17: Write a condition that accomplishes
 // the following logic:
 let res = 10;
@@ -73,3 +86,4 @@ let res = 10;
 // Step 18: Write a ternary that turns the light switch
 // off if it's on and on if it's off
 let lightSwitch = 'on';
+lightSwitch;
---
+++ Module-02/Arrays-and-Loops/main.js
@@ -0,0 +1,104 @@
+// Step 1: Assign the provide variable a 3 item array.
+// Make all 3 items favourite foods.
+let myFavFoods;
+
+// Step 2: Access the first item in the array
+let mffFirst;
+
+// Step 3: Access the last item in the array
+let mffLast;
+
+// Step 4: Open your console and copy and paste the
+// the message into the provided variable (NOTE: Computers
+// are literal, so copy&paste so you don't have a syntax error):
+let theConsoleMessage;
+
+// Step 5: Using the console.log syntax, output the
+// value of 'mySecretVar', and copy and paste the
+// the value in the following variable:
+let mySecretVarWas;
+
+// Step 6: Using the console.log, output the
+// array value 'mySecretArr'. Record how many
+// elements there are in the following variable:
+let numOfSecretElements;
+
+
+// For steps 7 to 12:
+// Nested Array Syntax Stuff
+let students = [
+  [56789, 56790, 56791],
+  ["Dave", "Bob", "Sally"],
+  ["Smith", "Alto", "Struthers"],
+  [
+    ["math", "science", "english"],
+    ["french", "science", "geography"],
+    ["music", "communications", "english"],
+  ],
+  [
+    [96, 53, 85],
+    [76, 83, 82],
+    [98, 21, 56],
+  ]
+];
+
+// Step 7: Access and store Dave's science grade.
+let davesSciGr;
+
+// Step 8: Access and store Bob's last name.
+let bobsLastName;
+
+// Step 9: Access and store Bob's geography grade.
+let bobsGeoGr;
+
+// Step 10: Using .length, how many elements does the
+// 'students' array have?
+let numOfStudents;
+
+// Step 11: Using .length, how many elements does the
+// 'students' first names array have?
+let numOfStudentsFirstNames;
+
+// Step 12: Using .length, how many elements does the
+// 'students' course list array have?
+let numOfStudentsCourses;
+
+
+// For steps 13 to 17
+let initArr;
+// Step 13: Using .push(), add the following 3 numbers
+// to the array: 4, 5, and 6.
+
+
+// Step 14: Using .unshift(), add the following 2 numbers
+// to the array: 7, 8.
+
+
+// Step 15: Using .pop(), remove the last number from the 
+// array.
+
+
+// Step 16: Using .shift(), remove the first number from
+// the array
+
+
+// Step 17: Using .splice(), add the number 256 between
+// 2 and 3
+
+
+// Step 18: Write a while loop that increments the
+// sentinel 8 times
+let sentinel = 0;
+
+
+// Step 19: Write a do while loop that increments the
+// sentinel 2 more times
+
+
+// Step 20: Write a for loop that adds 0 to 10 together
+let forLoopValue = 0;
+
+
+// Step 21: Write a for loop that adds 0 to 10 together
+let forOfLoopValue = 0;
+let exArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
